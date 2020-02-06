45b62af22716c2fd534cb3a0acfe6bf6b793b1ff
Achille Bavoua <bavouasoft@yahoo.fr>
2020-02-06 18:50:47 -0500

    updates files

--- models/Notes.js
+++ models/Notes.js
@@ -1,19 +1,7 @@
 let mongoose = require("mongoose");
-
-// Save a reference to the Schema constructor
 let Schema = mongoose.Schema;
-
-// Using the Schema constructor, create a new NoteSchema object
-// This is similar to a Sequelize model
-let NoteSchema = new Schema({
-  // `title` is of type String
-  title: String,
-  // `body` is of type String
-  body: String
+let noteSchema = new Schema({
+  note: String
 });
-
-// This creates our model from the above schema, using mongoose's model method
-let Note = mongoose.model("Note", NoteSchema);
-
-// Export the Note model
+let Note = mongoose.model("Note", noteSchema);
 module.exports = Note;
